a2003bdd7557ce86bd38dc53ff0e0d01e467e8f3
rluispdev <rluis@icloud.com>
2025-03-08 15:57:21 -0300

    Commit inicial Project 1

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Rafael Gonzaga on 3/8/25.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    var characteres: [Character] = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
