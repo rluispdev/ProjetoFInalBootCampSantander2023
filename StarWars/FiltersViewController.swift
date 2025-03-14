e554baa9c2ac34d752c12f240451aea067c1e05a
rluispdev <rluis@icloud.com>
2025-03-14 19:37:45 -0300

    aplicando filtro

--- StarWars/FiltersViewController.swift
+++ StarWars/FiltersViewController.swift
@@ -9,53 +9,61 @@ import UIKit
 protocol FiltersViewControllerDelegate: AnyObject {
     func didChangeFilter(gender: Gender)
 }
+ 
 
 class FiltersViewController: UIViewController {
-    
-    // Criação do UISegmentedControl programaticamente
+
     var genderSegmentControl: UISegmentedControl!
+    var filterButton: UIButton!
     
-    weak var delegate: FiltersViewControllerDelegate?
     var selectedGender: Gender = .all
+    weak var delegate: FiltersViewControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        print("FiltersViewController foi carregado.")
     }
     
     func setupUI() {
-        // Definir o título da tela
+        self.view.backgroundColor = .white
         title = "Filtrar Personagens"
         
-        // Criando e configurando o UISegmentedControl
+        // Criação do UISegmentedControl programaticamente
         genderSegmentControl = UISegmentedControl(items: Gender.allCases.map { $0.rawValue.capitalized })
         genderSegmentControl.selectedSegmentIndex = Gender.allCases.firstIndex(of: selectedGender) ?? 0
         genderSegmentControl.addTarget(self, action: #selector(filterChanged(_:)), for: .valueChanged)
         
-        // Adicionando o UISegmentedControl à view
-        genderSegmentControl.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(genderSegmentControl)
+        // Botão de filtro
+        filterButton = UIButton(type: .system)
+        filterButton.setTitle("Aplicar Filtro", for: .normal)
+        filterButton.addTarget(self, action: #selector(applyFilter), for: .touchUpInside)
         
-        // Definindo as constraints
+        // Adicionando o UISegmentedControl e o botão à view
+        genderSegmentControl.translatesAutoresizingMaskIntoConstraints = false
+        filterButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(genderSegmentControl)
+        self.view.addSubview(filterButton)
+        
+        // Definindo as constraints para o UISegmentedControl e o botão
         NSLayoutConstraint.activate([
-            genderSegmentControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            genderSegmentControl.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            genderSegmentControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            genderSegmentControl.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
             genderSegmentControl.widthAnchor.constraint(equalToConstant: 300),
-            genderSegmentControl.heightAnchor.constraint(equalToConstant: 40)
+            genderSegmentControl.heightAnchor.constraint(equalToConstant: 40),
+            
+            filterButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            filterButton.topAnchor.constraint(equalTo: genderSegmentControl.bottomAnchor, constant: 20)
         ])
     }
-    
+
     @objc func filterChanged(_ sender: UISegmentedControl) {
         let selectedGender = Gender.allCases[sender.selectedSegmentIndex]
-        print("Segmento selecionado: \(selectedGender.rawValue)")  // Verifique qual segmento foi selecionado
-        
-        delegate?.didChangeFilter(gender: selectedGender)  // Passa o filtro para o ViewController
-        navigationController?.popViewController(animated: true)  // Fecha o filtro
+        self.selectedGender = selectedGender
+        print("Filtro alterado para: \(selectedGender.rawValue)")
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("FiltersViewController está sendo fechado.")
+
+    @objc func applyFilter() {
+        delegate?.didChangeFilter(gender: selectedGender)
+        navigationController?.popViewController(animated: true)
     }
 }
--- StarWars/ViewController.swift
+++ StarWars/ViewController.swift
@@ -13,32 +13,44 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var tableView: UITableView!
    
+    
     var characteres: [Character] = []
-        var currentFilter: Gender = .all // Armazena o filtro atual
+    var currentFilter: Gender = .all
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Adicionando um botão de filtro à barra de navegação
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filtrar", style: .plain, target: self, action: #selector(showFilters))
+        
+        print("ViewController carregado.")
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        fetchData()  // Carregar os dados sem filtro inicialmente
+    }
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-
-            print("ViewController carregado.")
-
-            tableView.dataSource = self
-            tableView.delegate = self
-
-            fetchData()
-        }
-
+    // Função para exibir a tela de filtros
+    @objc func showFilters() {
+        let filtersVC = FiltersViewController()
+        filtersVC.delegate = self
+        filtersVC.selectedGender = currentFilter
+        navigationController?.pushViewController(filtersVC, animated: true)
+    }
+    
     func fetchData(gender: Gender = .all) {
         print("Iniciando fetch dos dados com filtro: \(gender.rawValue)")
-
+        
         var urlString = "https://swapi.dev/api/people/"
-
+        
         AF.request(urlString).responseJSON { response in
             switch response.result {
             case .success(let value):
                 print("Sucesso na requisição.")
-
+                
                 let decoder = JSONDecoder()
-
+                
                 if let data = try? JSONSerialization.data(withJSONObject: value, options: []) {
                     do {
                         let result = try decoder.decode(APIResponse.self, from: data)
@@ -52,7 +64,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                         
                         self.characteres = characters
                         print("Dados carregados: \(self.characteres.count) personagens.")
-
+                        
                         // Atualiza a tabela na thread principal
                         DispatchQueue.main.async {
                             print("Número de personagens após filtro: \(self.characteres.count)")
@@ -62,63 +74,42 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                         print("Erro ao decodificar os dados: \(error)")
                     }
                 }
-
+                
             case .failure(let error):
                 print("Erro na requisição: \(error)")
             }
         }
     }
 
-        // MARK: - UITableViewDataSource
+    // MARK: - UITableViewDataSource
 
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            print("Número de linhas na tabela: \(characteres.count)")
-            return characteres.count
-        }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("Número de linhas na tabela: \(characteres.count)")
+        return characteres.count
+    }
 
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell", for: indexPath)
-            let character = characteres[indexPath.row]
-            cell.textLabel?.text = character.name
-            return cell
-        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell", for: indexPath)
+        let character = characteres[indexPath.row]
+        cell.textLabel?.text = character.name
+        return cell
+    }
 
-        // MARK: - Navegação
+    // MARK: - Navegação
 
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if segue.identifier == "showFilters" {
-                if let filtersVC = segue.destination as? FiltersViewController {
-                    filtersVC.delegate = self
-                    filtersVC.selectedGender = currentFilter // Passa o filtro atual para a tela de filtros
-                    print("Abrindo FiltersViewController com filtro: \(currentFilter.rawValue)")
-                }
-            } else if segue.identifier == "showDetail" {
-                if let character = sender as? Character {
-                    if let detailVC = segue.destination as? DetailViewController {
-                        detailVC.character = character
-                        print("Abrindo detalhes do personagem: \(character.name)")
-                    }
-                }
-            }
-        }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCharacter = characteres[indexPath.row]
+        performSegue(withIdentifier: "showDetail", sender: selectedCharacter)
+    }
 
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            let selectedCharacter = characteres[indexPath.row]
-            performSegue(withIdentifier: "showDetail", sender: selectedCharacter)
-        }
+    // MARK: - FiltersViewControllerDelegate
 
-        // MARK: - FiltersViewControllerDelegate
-
-    func didChangeFilter( gender: Gender) {
+    func didChangeFilter(gender: Gender) {
         self.currentFilter = gender
-        fetchData() // Refaz a busca com o filtro atual
-
-        // Log para verificar se a filtragem está funcionando
-        print("Filtro alterado para: \(gender.rawValue)")
-        print("Número de personagens após filtragem: \(self.characteres.count)")
+        fetchData(gender: gender)
         
         DispatchQueue.main.async {
             self.tableView.reloadData()  // Recarrega a tabela
         }
     }
-    }
+}
